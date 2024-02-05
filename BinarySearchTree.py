8618b4516878856d8871441544a0d94062885190
Rahul Singh <142200955+Rahulsing12345@users.noreply.github.com>
2024-02-05 07:04:32 +0530

    Add files via upload

---
+++ BinarySearchTree.py
@@ -0,0 +1,18 @@
+class Node:
+
+    def __init__(self,left=None,item=None,right=None):
+        self.item = item
+        self.left = left
+        self.right = right
+
+class BST:
+# Create a root 
+    def __init__(self):
+        self.root = None
+
+# Create a method to INSERT an element
+    def insert(self, data):
+        pass
+
+
+    
